c1f58d744e7daf470227d9bb1674c97b3cd0d472
Sagiv Mishaan <sagiv505@gmail.com>
2022-05-22 01:04:31 +0300

    REPLACE - Account with AccountController and seperate web3 logic from ui

---
+++ Assets/Scripts/AccountController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using Nethereum.JsonRpc.UnityClient;
+
+public class AccountController : MonoBehaviour
+{
+
+    UIController uiController;
+	[DllImport("__Internal")] private static extern string WalletAddress();
+    public static string walletAddress = "0xac";
+    static string ethBalance = "00.00";
+
+    void Start()
+    {
+        walletAddress = WalletAddress();
+        StartCoroutine(getAccountBalance(walletAddress, SetEthBalance));
+        uiController = GameObject.FindWithTag("Player").GetComponent<UIController>();
+        uiController.setUIWalletAddress(walletAddress);
+	}
+
+    public string GetEthBalance() { return ethBalance; }
+
+    public string GetWalletAddress() { return walletAddress; }
+    public static IEnumerator getAccountBalance(string address, System.Action<decimal> callback)
+    {
+
+        var getBalanceRequest = new EthGetBalanceUnityRequest("https://mainnet.infura.io/v3/4679ded2f85c4d84b00e6cbd933df2cd");
+        yield return getBalanceRequest.SendRequest(address, Nethereum.RPC.Eth.DTOs.BlockParameter.CreateLatest());
+
+        if (getBalanceRequest.Exception == null)
+        {
+            var balance = getBalanceRequest.Result.Value;
+            callback(Nethereum.Util.UnitConversion.Convert.FromWei(balance, 18));
+        }
+        else
+        {
+            throw new System.InvalidOperationException("Get balance request failed");
+        }
+
+    }
+
+    public void SetEthBalance(decimal balance)
+    {
+        ethBalance = (balance).ToString();
+        uiController.setUIEthBalance(ethBalance);
+        uiController.GetEthBalance();
+    }
+
+
+
+
+
+}
